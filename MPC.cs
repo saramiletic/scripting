0b8d2e317f0826acc1e92c71996b2f625746ac55
saramiletic <150952293+saramiletic@users.noreply.github.com>
2023-11-22 18:50:33 +0100

    Add files via upload

---
+++ MPC.cs
@@ -0,0 +1,17 @@
+public class NCP : MonoBehaviour
+{
+    //NPC Health
+    int health = 5
+    
+    // NPC Level
+    int level = 1
+
+    //NPC Speed
+    public float speed = 1.2f
+
+    void Start ()
+    {
+
+    }
+     ...
+}
