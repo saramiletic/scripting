2e9736b2b2843aae61cba34a2953d0a58282f672
saramiletic <150952293+saramiletic@users.noreply.github.com>
2023-12-13 18:22:10 +0100

    Add files via upload

---
+++ SpeedTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTrigger : MonoBehaviour
+{
+    public float speedFactor = 2.5f
+
+        void OnTriggerEnter(Colider other)
+    {
+        other.GetComponent<FirstPersonMovement>().runSpeed *= speedFactor;
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<FirstPersonMovement>().runSpeedFactor;
+
+    }
+}
